11c5830c9ff26aa9b9a6a035cf2ad589500fded1
JakeTrannnn <111516444+JakeTrannnn@users.noreply.github.com>
2024-09-10 08:59:37 -0600

    coomit stuff

---
+++ Chapter2Notes.java
@@ -0,0 +1,25 @@
+
+class Chapter2Notes {
+    public static void main(String[] args) {
+        String theGreeting = "Hey There";
+        System.out.println(theGreeting);
+
+        String goodbye;
+        System.out.println(theGreeting);
+        System.out.println(theGreeting);
+        goodbye = "later";
+
+        System.out.println(goodbye);
+
+        int width = 50;
+
+        System.out.println(width);
+        int area = width * width;
+        System.out.println(area);
+        System.out.println(50 * 50);
+
+        int _sided_4;
+        String the_greeting;
+
+    }
+}
